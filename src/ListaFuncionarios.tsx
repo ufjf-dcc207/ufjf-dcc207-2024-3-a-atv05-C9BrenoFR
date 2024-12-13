949eeca0274702ae5761dae639d57ccdfdcce41f
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-12 22:42:23 -0300

    Preparação para exibir todos os funcionários

--- src/ListaFuncionarios.tsx
+++ src/ListaFuncionarios.tsx
@@ -1,8 +1,20 @@
+import "./ListaFuncionarios.css"
+import Funcionario from "./Funcionario";
+import json_info from "./funcionarios.json"
 
 export default function ListaFuncionarios(){
+    const funcionarios = json_info.funcionarios;
     return( 
-        <section className="ListaFuncionarios">
-
+        <section className="listaFuncionarios">
+            {funcionarios.map((funcionario, index) => (
+                <Funcionario
+                    key={index}
+                    nome={funcionario.nome_completo}
+                    setor={funcionario.setor}
+                    data_admissao={funcionario.data_admissao}
+                    salario_base={funcionario.salario_base}
+                />
+            ))}
         </section>
     )
 }
